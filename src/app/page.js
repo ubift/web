f1f8336e4c4a79c589528b24a20b6d787d9a21a0
ahmet <codingwithmish@gmail.com>
2024-04-21 17:55:18 +0300

    added hero component

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
+import Hero from './_components/Hero'
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <h2 className="font-bold text-[5rem] text-center">Unique websites designs with Ubift</h2>
-    </div>
+    <Hero />
   )
 }
