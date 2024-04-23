8cf5b289ffef76fa57971f590e6bf08b27d703a4
ahmet <codingwithmish@gmail.com>
2024-04-23 13:14:42 +0300

    added why-us page

---
+++ src/app/why-us/page.jsx
@@ -0,0 +1,19 @@
+export default function Page() {
+  return (
+    <div className='gap-12 flex items-center justify-center py-24'>
+      <div className='gap-6 flex flex-col items-center text-center mb-72'>
+        <h1 className='text-7xl font-extrabold'>Why Us?</h1>
+        <p className='text-primary-gray text-xl font-medium'>
+          Choose Ubift Team for seamless website creation effortlessly tailored to your needs.
+          Our intuitive tools enable users of all backgrounds to create stunning websites without the hassle of design or coding.
+          From freelancers to agencies and content creators, we offer versatile solutions to suit every project and budget.
+          Backed by a supportive community, we provide guidance, inspiration and help every step of the way. Our focus is on delivering professional results,
+          ensuring your website looks polished and effective. Experience the ease of website creation with Ubift Team today!
+        </p>
+        <button className='flex items-center justify-center bg-primary text-primary-dark px-10 py-3 rounded-md hover:opacity-70 transition-all'>
+          Contact
+        </button>
+      </div>
+    </div>
+  )
+}
